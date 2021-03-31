2922fcfb8e0e84da644f8cb3d30dc162878319e1
YaskurBearlyFernandes <12050111676@students.uin-suska.ac.id>
2021-03-31 14:40:21 +0700

    Tugas Pemrograman Lanjut Yaskur Bearly Fernandes

---
+++ src/basic/konsep/Apples.java
@@ -0,0 +1,8 @@
+package basic.konsep;
+
+public class Apples {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/basic/konsep/Appless.java
@@ -0,0 +1,9 @@
+package basic.konsep;
+
+public class Appless {
+    public static void main(String[] args) {
+        String name = "John";
+        int age = 24;
+        double height = 187.87;
+    }
+}
